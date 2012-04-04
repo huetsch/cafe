7579557da3ae696950ff9f263f791900dfea3c8f
Mark Huetsch <markhuetsch@gmail.com>
2012-04-04 14:37:38 +0800

    update compile to use yuicompressor

--- compile.sh
+++ compile.sh
@@ -2,3 +2,4 @@
 
 npm install asset-tag-helper && npm install cream && npm install date-helper && npm install url-helper
 browserify cafe.coffee -o browser-cafe.js
+yuicompressor browser-cafe.js > browser-cafe.min.js
